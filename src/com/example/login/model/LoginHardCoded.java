7d8dd7e2a5d38cd71ee6aa4d88c216605ddf01ed
Matheus Silva <matheus.silva@gastecnologia.com.br>
2015-03-13 10:47:08 -0300

    Novas credenciais

--- src/com/example/login/model/LoginHardCoded.java
+++ src/com/example/login/model/LoginHardCoded.java
@@ -6,10 +6,10 @@ public class LoginHardCoded implements LoginInterface {
 
 	@Override
 	public boolean isValidUser(User user) {
-		if(user.getUser().equals("user1") && user.getPass().equals("p1")) {
+		if(user.getUser().equals("u1") && user.getPass().equals("p1")) {
 			return true;
 		}
-		else if(user.getUser().equals("user2") && user.getPass().equals("p2")) {
+		else if(user.getUser().equals("u2") && user.getPass().equals("p2")) {
 			return true;
 		}
 		return false;
